d9cd37c25632ec891f41c52a4bc31c392ed30535
vinicios0202 <vinicius_serrano@estudante.sesisenai.org.br>
2021-12-02 16:38:04 -0300

    configuração de teste

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Card: React.FC = () => {
+    return (
+        <>
+            <h3>Card</h3>
+            <p>Guako o crocodilo com camisa</p>
+        </>
+    )
+}
+
+export default Card;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+import {Link} from 'react-router-dom';
+
+import Card from '../../components/Card';
+
 const Repository: React.FC = () => {
     return (
-        <h3>Pagina Repository</h3>
+        <>
+            <h3>Pagina Repository</h3>
+            <Link to={`/`}> Dashboard</Link>
+            <Card/>
+        </>
     );
 }
 export default Repository;
