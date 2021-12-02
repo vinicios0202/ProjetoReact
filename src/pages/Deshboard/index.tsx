0e37e8dbdcf57a7c9fd1e0708ce7aafdce04ec0e
vinicios0202 <vinicius_serrano@estudante.sesisenai.org.br>
2021-12-02 16:38:49 -0300

    configuração de tela dashboard

--- src/pages/Deshboard/index.tsx
+++ src/pages/Deshboard/index.tsx
@@ -1,8 +1,36 @@
 import React from "react";
+import {FiChevronRight} from 'react-icons/fi';
+
+import{Link} from 'react-router-dom';
+
+import {Title, Form, Repositories } from './Style';
+
+
 
 const Dashboard: React.FC = () => {
     return (
-        <h3>Pagina Dashboard</h3>
+        <>
+            <Title>Explore repositórios no github</Title>
+            <Form>
+                <input placeholder="Figite o nome  do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <Link to="#">
+                    <img src="https://www.gameinformer.com/sites/default/files/styles/thumbnail/public/2021/05/28/f73c4ea1/guapo.jpg" alt="" />
+                    <div>
+                        <strong>ProjetoReactMalwee</strong>
+                        <p>glubiglubiglubi</p>
+                        <button type="submit"></button>
+                    </div>
+                    <FiChevronRight/>
+
+                </Link>
+            </Repositories>
+
+            
+        </>
     );
 }
 
