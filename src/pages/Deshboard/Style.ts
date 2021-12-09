70e17d97a491dd81ad27798c52aee3efabcec969
vinicios0202 <vinicius_serrano@estudante.sesisenai.org.br>
2021-12-09 16:27:39 -0300

    Projeto React

--- src/pages/Deshboard/Style.ts
+++ src/pages/Deshboard/Style.ts
@@ -43,5 +43,63 @@ export const Form = styled.form`
 
 export const Repositories = styled.div`
     margin-top: 80px;
-    max- width: 700px
+    max-width: 700px;
+
+
+    a {
+        background: #fff;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+
+        display: flex;
+        align-items: center;
+
+        transition: transform 0.2s;
+
+        &:hover {
+            transform: translate(10px);
+        }
+
+        & + a {
+            margin-top: 16px;
+        }
+
+        img{
+            width: 64px;
+            heigth: 65px;
+            border-radius: 50%;
+        }
+
+        div{
+            margin: 0 16px;
+            flex: 1;
+
+            strong{
+                font-size: 20px;
+                color: #3d3d4d;
+            }
+
+            p{
+                font=size: 18px;
+                color: #a8a8d3;
+                margin-top: 4px;
+            }
+        }
+
+        & + &{
+            margin-top: 16px;
+
+        }
+    }
+
+
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
--- src/pages/Deshboard/index.tsx
+++ src/pages/Deshboard/index.tsx
@@ -1,32 +1,101 @@
-import React from "react";
+import React, {useState, useEffect, FormEvent} from "react";
 import {FiChevronRight} from 'react-icons/fi';
 
 import{Link} from 'react-router-dom';
 
-import {Title, Form, Repositories } from './Style';
+import api from '../../services/api';
+
+import {Title, Form, Repositories, Error } from './Style';
+
+interface Repository{
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+
+    }
+}
 
 
 
 const Dashboard: React.FC = () => {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepository = localStorage.getItem(
+            '@GithubExplorer:repositories',
+
+        );
+        
+        if(storageRepository) {
+            return JSON.parse(storageRepository);
+
+        }
+
+
+        return[];
+    });
+
+    const handleAddRepository = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(!newRepo){
+            setInputError("Digite um usuario/repositório para pesquisar.")
+            return;
+        }
+
+    try {
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository])
+        setNewRepo('');
+        setInputError('');
+
+        } catch(err){
+            setInputError("Repositório não encontrado ou inexistente.");
+            
+
+        }
+
+    }
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories)
+        )
+
+    }, [repositories]);
+
     return (
         <>
             <Title>Explore repositórios no github</Title>
-            <Form>
-                <input placeholder="Figite o nome  do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    onChange = {e => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome  do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {inputError && <Error>{inputError}</Error>}
+
             <Repositories>
+                {repositories.map(repository => (
                 <Link to="#">
-                    <img src="https://www.gameinformer.com/sites/default/files/styles/thumbnail/public/2021/05/28/f73c4ea1/guapo.jpg" alt="" />
+                    <img src={repository.owner.avatar_url} alt="repository.owner.login" />
                     <div>
-                        <strong>ProjetoReactMalwee</strong>
-                        <p>glubiglubiglubi</p>
+                        <strong>{repository.full_name} </strong>
+                        <p>{repository.description}</p>
                         <button type="submit"></button>
                     </div>
-                    <FiChevronRight/>
+                    <FiChevronRight size={20}/>
 
                 </Link>
+                ))}
             </Repositories>
 
             
